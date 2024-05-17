3d699cc6d36c74868a291cd5b975f1cd4ce97a48
Andrea Santoro <andrea.santoro@epfl.ch>
2024-05-17 16:25:31 +0200

    add scripts for generating results and figure 3

---
+++ Figure3/Code/01_triangles_scaffold_computation_rest_HCP/launch_HOlocal_indicators_day2.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+current_dir=`pwd`
+code_dir="./../../../Core_code/"
+ncores=90
+output_folder="/home/asantoro/COST/Project2_Higher_Order_Brain/Brain_HORS/Figure3/Results/"
+#file_input_format="TS_Schaefer100S_gsr_bp_z_rest300_tasks_EMO_GAM_LAN_MOT_REL_SOC_WM"
+
+
+
+cd ${code_dir}
+
+for subjID in `ls -d /home/asantoro/HCP/* | cut -f5 -d/` ## Loop through the subjects
+do
+	echo ${subjID}
+	file_input="/home/asantoro/HCP/${subjID}/rfMRI_REST2_LR/Schaefer100/TS_Schaefer100S_gsr_bp_z.mat"
+	sh launch_julia_HOlocalinfo_all.sh ${ncores} ${file_input} ${output_folder}HO_scaffold_${subjID}_REST2_LR ${output_folder}HO_triangles_${subjID}_REST2_LR ${output_folder}HO_indicators_${subjID}_REST2_LR.txt
+
+	file_input="/home/asantoro/HCP/${subjID}/rfMRI_REST2_RL/Schaefer100/TS_Schaefer100S_gsr_bp_z.mat"
+	sh launch_julia_HOlocalinfo_all.sh ${ncores} ${file_input} ${output_folder}HO_scaffold_${subjID}_REST2_RL ${output_folder}HO_triangles_${subjID}_REST2_RL ${output_folder}HO_indicators_${subjID}_REST2_RL.txt
+
+
+done
+
+cd ${current_dir}
+
---
+++ Figure3/Code/01_triangles_scaffold_computation_rest_HCP/launch_HOlocal_indicators_day2_redo.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+current_dir=`pwd`
+code_dir="./../../../Core_code/"
+ncores=90
+output_folder="/home/asantoro/COST/Project2_Higher_Order_Brain/Brain_HORS/Figure3/Results/"
+#file_input_format="TS_Schaefer100S_gsr_bp_z_rest300_tasks_EMO_GAM_LAN_MOT_REL_SOC_WM"
+
+
+
+cd ${code_dir}
+
+for subjID in `ls -d /home/asantoro/HCP/* | cut -f5 -d/` ## Loop through the subjects
+do
+	#echo ${subjID}
+#	file_input="/home/asantoro/HCP/${subjID}/${file_input_format}_LR.mat"
+	file_input="/home/asantoro/HCP/${subjID}/rfMRI_REST2_LR/Schaefer100/TS_Schaefer100S_gsr_bp_z.mat"
+	file_byte=`wc --bytes ${output_folder}HO_indicators_${subjID}_REST2_LR.txt | cut -d" " -f1`
+	#echo ${file_byte}
+	if [ ! -f ${output_folder}HO_indicators_${subjID}_REST2_LR.txt ] || [[ ${file_byte} -eq 0 ]]
+	then 
+		echo ${subjID} "LR"
+		sh launch_julia_HOlocalinfo_all.sh ${ncores} ${file_input} ${output_folder}HO_scaffold_${subjID}_REST2_LR ${output_folder}HO_triangles_${subjID}_REST2_LR ${output_folder}HO_indicators_${subjID}_REST2_LR.txt
+	fi
+	
+	
+
+
+
+	file_input="/home/asantoro/HCP/${subjID}/rfMRI_REST2_RL/Schaefer100/TS_Schaefer100S_gsr_bp_z.mat"
+	file_byte=`wc --bytes ${output_folder}HO_indicators_${subjID}_REST2_RL.txt | cut -d" " -f1`
+	if [ ! -f ${output_folder}HO_indicators_${subjID}_REST2_RL.txt ] || [[ ${file_byte} -eq 0 ]]
+	then
+		echo ${subjID} "RL"
+		sh launch_julia_HOlocalinfo_all.sh ${ncores} ${file_input} ${output_folder}HO_scaffold_${subjID}_REST2_RL ${output_folder}HO_triangles_${subjID}_REST2_RL ${output_folder}HO_indicators_${subjID}_REST2_RL.txt
+	fi
+done
+
+cd ${current_dir}
+
